ad733f28cf4b53bfb1382fca5195eb0160641787
UdayKalra <uday.kalra@outlook.com>
2015-06-28 21:59:21 -0700

    E_fifteen

---
+++ Homework1_Uday/E_Fifteen.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+public class E_Fifteen
+{
+     public static void main(String[] args){
+        String name = JOptionPane.showInputDialog("What is your name?");
+        System.out.print("Hello, ");
+        System.out.print(name);    
+    }
+}
